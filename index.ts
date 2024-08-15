f8ad31b91d97338a76f1afe718dee68930d504b0
Mio Nekoya <admin@mio9.sh>
2024-08-16 02:14:42 +0900

    add discordjs

--- index.ts
+++ index.ts
@@ -1,4 +1,57 @@
-import wretch from "wretch"
+import { Ollama } from "./lib/ollama";
+import { REST, Routes } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
 
+const env = {
+    discordToken: process.env.DISCORD_TOKEN as string,
+    clientId: process.env.DISCORD_CLIENT_ID as string
+}
 
-const ollama = wretch("http://localhost:11434/api/generate")
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(env.discordToken);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationCommands(env.clientId), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(env.discordToken);
+
+const ollama = new Ollama({
+    // model: "wizard-vicuna-uncensored:7b",
+    model: "llama3.1:8b",
+    // system: "Insert emojis in between words for following sentence, answer with result only.\n Input:\n",
+    system: "Add several emojis between each words. Ass as much emoji as necessary. There can be 2 or more emojis in between words. Answer with result only",
+    options : {
+        mirostat: 2
+    }
+})
+
+const result = await ollama.generate("")
+
+console.log(result.response)
--- lib/ollama.ts
+++ lib/ollama.ts
@@ -1,19 +1,51 @@
-export default function ollama(config: OllamaConfig ): OllamaInstance {
-    return new OllamaInstance(config)
+import wretch, { type Wretch } from "wretch"
 
+export interface OllamaResponse {
+    model:                string;
+    created_at:           Date;
+    response:             string;
+    done:                 boolean;
+    context:              number[];
+    total_duration:       number;
+    load_duration:        number;
+    prompt_eval_count:    number;
+    prompt_eval_duration: number;
+    eval_count:           number;
+    eval_duration:        number;
 }
+
 
 export interface OllamaConfig {
-    url: string
+    url?: string
     model: string
+    system?: string
+    options?: any
 }
 
-export class OllamaInstance {  
-    _url: string
-    _model: string
+export class Ollama {  
+    url: string
+    model: string
+    system?: string
+    private w: Wretch
+    options?: any
 
     constructor(config: OllamaConfig) {
-        this._url = config.url
-        this._model = config.model
+        this.url = config.url || "http://localhost:11434"
+        this.model = config.model
+        this.system = config.system
+        this.options = config.options
+
+        this.w = wretch(this.url)
+    }
+
+    async generate(prompt: string): Promise<OllamaResponse>{
+        const result = this.w.url(`/api/generate`).post({
+            model: this.model,
+            prompt,
+            stream: false,
+            system: this.system
+        }).json<OllamaResponse>()
+        
+        return result
     }
  }
